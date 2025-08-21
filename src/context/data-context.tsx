b73cea57bc77f4be8402d61ac7a4a93cf2a3f4f0
Rogerio DAS NEVES NUNES <walckerover@gmail.com>
2025-08-21 03:30:48 +0000

    então vamos reduzir o lote para 1 e ver se sai dos 50%

--- src/context/data-context.tsx
+++ src/context/data-context.tsx
@@ -81,7 +81,7 @@ const setStorageItem = (key: string, value: any) => {
     }
 };
 
-const BATCH_SIZE = 5;
+const BATCH_SIZE = 1;
 
 export const DataProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProductsState] = useState<Product[]>([]);
